b5249baaf636dc2dd1846fbd4cb666c844503e47
emailsbecker <63986025+emailsbecker@users.noreply.github.com>
2020-06-16 00:33:55 -0400

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,46 +1,46 @@
 import subprocess
 
-with open("sitenames.txt", 'r') as f:
-    for l in f:
-        varURL = l.rstrip()
+with open("sitenames.txt", 'r') as INPUTFILE:
+    for LINE in INPUTFILE:
+        URL = LINE.rstrip()
         print()
-        print("varURL is: ",l)
+        print("URL is: ",LINE)
         print('----------------------------------------')
 
-        nslresult = subprocess.run(['nslookup', varURL], capture_output=True, universal_newlines=True)
+        NSLRESULT = subprocess.run(['nslookup', URL], capture_output=True, universal_newlines=True)
         print()
-        print("nslresult is:")
+        print("NSLRESULT is:")
         print()
-        print(nslresult.stdout)
+        print(NSLRESULT.stdout)
         print('----------------------------------------')
 
-        nslout = nslresult.stdout.split( )
+        NSLOUT = NSLRESULT.stdout.split( )
         print("The first 9 items in the result (split by spaces) are:")
         print()
-        print (nslout[0])
-        print (nslout[1])
-        print (nslout[2])
-        print (nslout[3])
-        print (nslout[4])
-        print (nslout[5])
-        print (nslout[6])
-        print (nslout[7])
-        print (nslout[8])
-        print (nslout[9])
+        print (NSLOUT[0])
+        print (NSLOUT[1])
+        print (NSLOUT[2])
+        print (NSLOUT[3])
+        print (NSLOUT[4])
+        print (NSLOUT[5])
+        print (NSLOUT[6])
+        print (NSLOUT[7])
+        print (NSLOUT[8])
+        print (NSLOUT[9])
         print('----------------------------------------')
 
-        ip = nslout[9]
-        pingresult = subprocess.run(['ping', '-c', '1', ip], capture_output=True, universal_newlines=True)
-        print(pingresult.stdout)
+        IPADDR = NSLOUT[9]
+        PINGRESULT = subprocess.run(['ping', '-c', '1', IPADDR], capture_output=True, universal_newlines=True)
+        print(PINGRESULT.stdout)
         print('----------------------------------------')
 
-        if str.isdigit(ip[0]) == True:
-            first_char = int(ip[0])
+        if str.isdigit(IPADDR[0]) == True:
+            FIRST_CHAR = int(IPADDR[0])
         else:
-            first_char = ip[0]
+            FIRST_CHAR = IPADDR[0]
 
-        print("The first character in the ip variable is:",ip[0])
-        switcher={
+        print("The first character in the ip variable is:",IPADDR[0])
+        SELECTION={
             1: 'The first character in the IP address is a 1.',
             2: 'The first character in the IP address is a 2.',
             3: 'The first character in the IP address is a 3.',
@@ -51,13 +51,13 @@ with open("sitenames.txt", 'r') as f:
             8: 'The first character in the IP address is a 8.',
             9: 'The first character in the IP address is a 9.'
         }
-        observation = switcher.get(first_char,"The first character in that variable is not a number.")
-        print(observation)
+        OBSERVATION = SELECTION.get(FIRST_CHAR,"The first character in that variable is not a number.")
+        print(OBSERVATION)
         print('----------------------------------------')
 
-        OutputFile = open("outputfile.txt", "a")
-        towrite = varURL + "," + ip + "," + observation + "\n"
-        OutputFile.write(towrite)
-        OutputFile.close()
+        OUTPUTFILE = open("outputfile.txt", "a")
+        TO_WRITE = URL + "," + IPADDR + "," + OBSERVATION + "\n"
+        OUTPUTFILE.write(TO_WRITE)
+        OUTPUTFILE.close()
 
-f.close()
+INPUTFILE.close()
